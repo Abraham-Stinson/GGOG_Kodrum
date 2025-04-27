7b5aaf180d0049eb504159383bad15d78a6117c9
İbrahim Gümüşdal <ibooo2977@gmail.com>
2025-04-27 14:05:31 +0300

    Revert "UI Score Bar and Main Menu Added"
    This reverts commit 60e7510abc733dbf5c611d5184c17a7ee0703cbe.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
@@ -10,13 +9,8 @@ public class ScoreManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private int p1Score = 0;
     private int p2Score = 0;
-    [SerializeField] private Image[] p1Bars;
-    [SerializeField] private Image[] p2Bars;
-    public Sprite p1EmptySprite;
-    public Sprite p1FullSprite;
-    public Sprite p2EmptySprite;
-    public Sprite p2FullSprite;
-    
+    [SerializeField] private TextMeshProUGUI p1ScoreText;
+    [SerializeField] private TextMeshProUGUI p2ScoreText;
     [SerializeField] private GameObject winScreen;
     [SerializeField] private TextMeshProUGUI resultText;
     [SerializeField] private float endScreenLoadTime;
@@ -41,36 +35,21 @@ public class ScoreManager : MonoBehaviour
     }
     void RefreshScoreUI()
     {
-        for(int i = 0; i<p1Bars.Length;i++){
-            if(i<p1Score){
-                p1Bars[i].sprite=p1FullSprite;
-            }
-            else{
-                p1Bars[i].sprite=p1EmptySprite;
-            }
-        }
-        for(int i = 0; i<p2Bars.Length;i++){
-            if(i<p2Score){
-                p2Bars[i].sprite=p2FullSprite;
-            }
-            else{
-                p2Bars[i].sprite=p2EmptySprite;
-            }
-        }
-        
+        p1ScoreText.text = "Red Score: " + p1Score.ToString();
+        p2ScoreText.text = "Blue Score: " + p2Score.ToString();
     }
 
     void WinCondition()
     {
         
-        if (p1Score >= 3)
+        if (p1Score >= 5)
         {
             resultText.text = "Red Player Wins!";
             StartCoroutine(WaitAndLoad());
             playerControllerScript.enabled = false;
             winScreen.SetActive(true);
         }
-        else if (p2Score >= 3)
+        else if (p2Score >= 5)
         {
             resultText.text = "Blue Player Wins!";
             StartCoroutine(WaitAndLoad());
